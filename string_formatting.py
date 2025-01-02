2773021e9d95c5822db3e10896a81fb312b54f15
clutch_god17 <devadigaajay1729@gmail.com>
2025-01-03 00:42:12 +0530

    string formatting.

---
+++ string_formatting.py
@@ -0,0 +1,12 @@
+# different types of string
+name = 'Alice'
+age = 25
+
+# using f-strings
+print(f'Hello, my name is {name} and I am {age} years old.')
+
+# using format() function
+print('Hello, my name is {} and I am {} years old.'.format(name, age))
+
+# using % formatting
+print('Hello, my name is %s and I am %d years old.' % (name, age))
