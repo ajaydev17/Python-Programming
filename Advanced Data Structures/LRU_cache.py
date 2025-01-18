299188229b38e7886839b35c261d3b08baacd625
clutch_god17 <devadigaajay1729@gmail.com>
2025-01-18 14:16:34 +0530

    LRU cache example.

---
+++ Advanced Data Structures/LRU_cache.py
@@ -0,0 +1,25 @@
+"""
+An LRU Cache evicts the least recently accessed item when capacity is reached.
+Using OrderedDict, Python’s LRU implementation is efficient, with O(1) for put and get.
+"""
+
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
