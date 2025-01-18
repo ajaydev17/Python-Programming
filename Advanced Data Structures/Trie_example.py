eddec9166b1daffb9e004ffd4240413ba30feae5
clutch_god17 <devadigaajay1729@gmail.com>
2025-01-18 14:17:46 +0530

    trie example.

---
+++ Advanced Data Structures/Trie_example.py
@@ -0,0 +1,31 @@
+"""
+A trie (or prefix tree) is a tree-like structure for efficient string searches, especially
+suited for applications like autocomplete and spell-checking. Each node represents a
+character, and paths from the root to the leaves form words.
+"""
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
