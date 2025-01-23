3f6990939fe7e1b2ab0f74d439961f96325f6d70
clutch_god17 <devadigaajay1729@gmail.com>
2025-01-23 16:45:41 +0530

    database concepts.

---
+++ Database Operations/database_concepts.py
@@ -0,0 +1,17 @@
+"""
+1. Why mongodb?
+2. Difference between SQL and No SQL.
+3. How indexing works and what's to do and what's not to do.
+4. Scaling
+5. Sharding
+6. Replication
+7. Caching
+8. RedisDB
+9. How DB works in Microservices
+10. Queuing
+11. Basics of SQL
+12. ACID principle
+13. Normalization and Renormalization
+14. CAP theorem
+15. Sharding with Horizontal scaling how the hashing will work
+"""
